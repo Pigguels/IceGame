a7b77d3001d1b98b646e1f4a5e3c834911a23517
Harley Bekker <harleybekker@outlook.com>
2022-07-09 00:40:05 +1000

    Fixed: ice pools collision, Added framework for players climb

---
+++ Ice Game/Assets/Scripts/IcePool.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(BoxCollider))]
+public class IcePool : MonoBehaviour
+{
+    public PlayerController m_PlayerCon;
+
+    public float m_SlowAmount = 0.0f;
+    public float m_SlowTimePerSecond = 1.0f;
+    public float m_RecoverTimePerSecond = 1.0f;
+
+    private bool m_PlayerInside = false;
+
+    private BoxCollider m_Box;
+
+    void Start()
+    {
+        m_Box = gameObject.GetComponent<BoxCollider>();
+
+        if (!m_PlayerCon)
+        {
+            m_PlayerCon = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        bool playerHit = IsPlayerInside();
+
+        if (playerHit && !m_PlayerInside)
+        {
+            /* Player entered */
+            m_PlayerCon.Slow(m_SlowAmount, m_SlowTimePerSecond);
+        }
+        else if (!playerHit && m_PlayerInside)
+        {
+            /* Player exited */
+            m_PlayerCon.Slow(-m_SlowAmount, m_RecoverTimePerSecond);
+        }
+
+        m_PlayerInside = playerHit;
+    }
+
+    /// <summary>
+    /// Returns true if the players controller collider is within this objects box collider
+    /// </summary>
+    private bool IsPlayerInside()
+    {
+        Collider[] hits = Physics.OverlapBox(transform.position + m_Box.center, new Vector3(transform.localScale.x * m_Box.size.x, transform.localScale.y * m_Box.size.y, transform.localScale.z * m_Box.size.z) * 0.5f);
+        foreach (Collider col in hits)
+            if (col.CompareTag("Player"))
+                return true;
+        return false;
+    }
+}
--- Ice Game/Assets/Scripts/PlayerController.cs
+++ Ice Game/Assets/Scripts/PlayerController.cs
@@ -4,34 +4,93 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    #region Basic Movement Variables
 
+    [Header("Basic Movement:")]
     public float m_WalkSpeed = 6.5f;
-    private float m_MoveSpeed;
+    public float m_MinMoveSpeed = 0.1f;
+    public float m_AirControl = 0.5f;
+
+    private float m_CurrentMoveSpeed = 0.0f;
+    private float m_TargetMoveSpeed = 0.0f;
+    private float m_MoveSpeedAdjustSpeed = 1.0f;
+    
+    [Space]
+    public float m_MaxSlopeAngle = 60f;
+    public float m_SlopeAdustmentDistance = 0.2f;
     private Vector3 m_MoveDir;
+    private Vector3 m_PreviousMoveDir;
 
-    public float m_JumpHeight = 1.5f;
-
+    [Space]
     public float m_Gravity = 9.81f;
     public float m_Drag = 1f;
     private Vector3 m_Velocity;
 
     private bool m_IsGrounded;
 
-    public float m_LookSensitivity = 4f;
+    public enum MovementStates { walk, crouch, climb }
+    private MovementStates m_MoveState;
+
+    #endregion
+
+    #region Crouch Variables
+
+    [Header("Crouching:")]
+    public KeyCode m_CrouchKey = KeyCode.LeftControl;
+    [Space]
+    public float m_CrouchMoveSpeed = 2.5f;
+    public float m_VerticalCrouchSpeed = 0.5f;
+    [Space]
+    public float m_StandingHeight = 2f;
+    public float m_CrouchHeight = 1f;
+    public float m_EyeLevel = 1.75f;
+
+    private float m_TargetHeight = 2f;
+
+    #endregion
+
+    #region Climb Variables
+
+    [Header("Climbing:")]
+    public KeyCode m_ClimbKey = KeyCode.F;
+
+    public float m_DistanceToStartClimb = 0.5f;
+
+    #endregion
+
+    #region Camera variables
+
+    [Header("Camera:")]
+    public float m_LookSensitivity = 200f;
     [Range(0f, 90f)]
-    public float m_PitchClamp = 60f;
+    public float m_UpwardPitchClamp = 60f;
+    [Range(0f, 90f)]
+    public float m_DownwardPitchClamp = 60f;
     private float m_Pitch = 0f;
     private float m_Yaw = 0f;
-
     public Transform m_Head;
 
+    #endregion
+
     private CharacterController m_CharController;
+
+    private LayerMask m_IgnoreMask;
 
     void Start()
     {
         m_CharController = GetComponent<CharacterController>();
 
-        m_MoveSpeed = m_WalkSpeed;
+        m_IgnoreMask = LayerMask.GetMask("PlayerCheckIgnore");
+
+        m_TargetHeight = m_StandingHeight;
+
+        m_CurrentMoveSpeed = m_WalkSpeed;
+        m_TargetMoveSpeed = m_WalkSpeed;
+        m_MoveSpeedAdjustSpeed = 0.0f;
+        m_MoveDir = Vector3.zero;
+        m_PreviousMoveDir = Vector3.zero;
+
+        m_MoveState = MovementStates.walk;
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -39,18 +98,30 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        m_PreviousMoveDir = m_MoveDir;
         m_MoveDir = (transform.right * Input.GetAxisRaw("Horizontal") + transform.forward * Input.GetAxisRaw("Vertical")).normalized;
 
         m_IsGrounded = IsGrounded();
 
         ApplyPhysics();
 
-        if (m_IsGrounded && Input.GetKeyDown(KeyCode.Space))
-        {
-            m_Velocity.y += Mathf.Sqrt(-m_JumpHeight / 2f * -m_Gravity) * 2f;
-        }
+        MoveSpeedAdjustment();
+        AdjustYScale(m_TargetHeight, m_VerticalCrouchSpeed);
+        SlopeAdjustment();
 
-        Walk();
+        switch (m_MoveState)
+        {
+            case MovementStates.walk:
+                Walk();
+                break;
+
+            case MovementStates.crouch:
+                Crouch();
+                break;
+
+            case MovementStates.climb:
+                break;
+        }
     }
 
     private void LateUpdate()
@@ -58,36 +129,192 @@ public class PlayerController : MonoBehaviour
         Look();
     }
 
+    #region Checks
+
+    /// <summary>
+    /// Returns true if the player is on ground
+    /// </summary>
     private bool IsGrounded()
     {
         RaycastHit sphereHit;
-        return Physics.SphereCast(transform.position + m_CharController.center, m_CharController.radius, Vector3.down, out sphereHit, (m_CharController.height * 0.5f) - m_CharController.radius + m_CharController.skinWidth + 0.01f, ~gameObject.layer);
+        return Physics.SphereCast(transform.position + m_CharController.center, m_CharController.radius, Vector3.down, out sphereHit, (m_CharController.height * 0.5f) - m_CharController.radius + m_CharController.skinWidth + 0.01f, ~gameObject.layer & ~m_IgnoreMask);
     }
 
+    /// <summary>
+    /// Returns true if there is enough room above character controller to stand,calso returns true if already standing
+    /// </summary>
+    private bool CanStand()
+    {
+        if (m_TargetHeight == m_StandingHeight)
+            return true;
+
+        RaycastHit sphereHit;
+        return !Physics.SphereCast(transform.position + m_CharController.center, m_CharController.radius, Vector3.up, out sphereHit, (m_CharController.height * 0.5f) + (m_StandingHeight - m_CharController.height) - m_CharController.radius + m_CharController.skinWidth, ~gameObject.layer & ~m_IgnoreMask);
+    }
+
+    /// <summary>
+    /// Returns true if the player can climb
+    /// </summary>
+    private bool CanClimb()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, m_CharController.radius + m_DistanceToStartClimb, ~m_IgnoreMask))
+        {
+            if (hit.collider.CompareTag("Climbable"))
+                return true;
+        }
+        return false;
+    }
+
+    #endregion
+
+    #region Adjustments
+
+    /// <summary>
+    /// Applies gravity and drag to the velocity
+    /// </summary>
     private void ApplyPhysics()
     {
+        /* Apply gravity to the velocity */
         if (m_IsGrounded)
             m_Velocity.y = 0f;
         else if (m_Velocity.y > -(m_Gravity * m_Gravity))
             m_Velocity.y -= m_Gravity * Time.deltaTime;
 
+        /* Apply drag to the velocity */
         m_Velocity.x = Mathf.MoveTowards(m_Velocity.x, 0f, m_Drag);
         m_Velocity.z = Mathf.MoveTowards(m_Velocity.z, 0f, m_Drag);
     }
 
+    /// <summary>
+    /// Slope adjustment to keep the player grounded when they walk
+    /// </summary>
+    private void SlopeAdjustment()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, m_CharController.height * 0.5f + m_SlopeAdustmentDistance, ~gameObject.layer))
+        {
+            if (Mathf.Acos(Vector3.Dot(hit.normal, Vector3.up)) < m_MaxSlopeAngle * Mathf.Deg2Rad)
+            {
+                m_Velocity.y = -(m_Gravity * m_Gravity);
+            }
+        }
+    }
+    
+    /// <summary>
+    /// Adjust the players current move speed to the target move speed
+    /// </summary>
+    private void MoveSpeedAdjustment()
+    {
+        m_CurrentMoveSpeed = Mathf.MoveTowards(m_CurrentMoveSpeed, m_TargetMoveSpeed, m_MoveSpeedAdjustSpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Lerp the character controllers y scale to the height and adjust the position to make it seamless
+    /// </summary>
+    private void AdjustYScale(float height, float speed)
+    {
+        m_CharController.enabled = false;
+
+        float controllerHeightCache = m_CharController.height;
+
+        /* Change the character controllers height */
+        m_CharController.height = Mathf.Lerp(controllerHeightCache, height, speed);
+
+        /* Offset position */
+        float yOffset = (Mathf.Lerp(controllerHeightCache, height, speed) - controllerHeightCache) * 0.5f;
+        m_CharController.center += new Vector3(0f, yOffset, 0f);
+
+        /* Adjust eye level */
+        m_Head.localPosition = new Vector3(m_Head.localPosition.x, m_EyeLevel * m_CharController.height * 0.5f, m_Head.localPosition.z);
+
+        m_CharController.enabled = true;
+    }
+
+    #endregion
+
+    /// <summary>
+    /// The players look rotation and control
+    /// </summary>
     private void Look()
     {
         m_Pitch -= Input.GetAxis("Mouse Y") * m_LookSensitivity * Time.deltaTime;
         m_Yaw += Input.GetAxis("Mouse X") * m_LookSensitivity * Time.deltaTime;
 
-        m_Pitch = Mathf.Clamp(m_Pitch, -m_PitchClamp, m_PitchClamp);
+        m_Pitch = Mathf.Clamp(m_Pitch, -m_DownwardPitchClamp, m_UpwardPitchClamp);
 
         m_Head.localRotation = Quaternion.Euler(m_Pitch, 0f, 0f);
         transform.localRotation = Quaternion.Euler(0f, m_Yaw, 0f);
     }
 
+    #region Movestates
+
+    /// <summary>
+    /// The players direction movement and slope handling
+    /// </summary>
     private void Walk()
     {
-        m_CharController.Move(((m_MoveDir * m_MoveSpeed) + m_Velocity) * Time.deltaTime);
+        /* Walk in the desired direction */
+        if (m_IsGrounded)
+            m_CharController.Move(((m_MoveDir * m_CurrentMoveSpeed) + m_Velocity) * Time.deltaTime);
+        else
+            m_CharController.Move(((m_PreviousMoveDir * m_TargetMoveSpeed * m_AirControl) + m_Velocity) * Time.deltaTime);
+
+        /* Switch to crouch */
+        if (m_IsGrounded && Input.GetKeyDown(m_CrouchKey))
+        {
+            m_TargetHeight = m_CrouchHeight;
+            m_MoveState = MovementStates.crouch;
+        }
+    }
+
+    /// <summary>
+    /// The players crouch state
+    /// </summary>
+    private void Crouch()
+    {
+        /* Exit the crouch state */
+        if (!Input.GetKey(m_CrouchKey) && CanStand())
+        {
+            m_TargetHeight = m_StandingHeight;
+            m_MoveState = MovementStates.walk;
+            return;
+        }
+
+        /* If the players grounded move them in their desired direction */
+        if (m_IsGrounded)
+            m_CharController.Move((m_MoveDir * m_CrouchMoveSpeed + m_Velocity) * Time.deltaTime);
+        else
+        {
+            /* If player tries to move move them in tht direction, if not continue in there last move direction */
+            if (m_MoveDir != Vector3.zero)
+                m_CharController.Move((m_MoveDir * m_CrouchMoveSpeed * m_AirControl + m_Velocity) * Time.deltaTime);
+            else
+                m_CharController.Move((m_PreviousMoveDir * m_CrouchMoveSpeed * m_AirControl + m_Velocity) * Time.deltaTime);
+        }
+    }
+
+    /// <summary>
+    /// The players climb state
+    /// </summary>
+    private void Climb()
+    {
+
+    }
+
+    #endregion
+
+    /// <summary>
+    /// Slows the players movement speed by the given amount over the given time
+    /// </summary>
+    public void Slow(float amount, float time)
+    {
+        m_TargetMoveSpeed -= amount;
+        m_MoveSpeedAdjustSpeed = time;
+
+        if (m_TargetMoveSpeed < m_MinMoveSpeed)
+        {
+            m_TargetMoveSpeed = m_MinMoveSpeed;
+        }
     }
 }
